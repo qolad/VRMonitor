1f091d0e970c09c407617848e37bfdfda6b52a22
haiazi <haiazipooks@outlook.com>
2026-09-13 05:22:16 +0000

    Add OpenVR SDK

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <openvr.h>
+
 
 int main (void){
     std::cout << "VR Monitor starting...\n";
 
+
+    std::cout << "openvr version: " << vr::k_nSteamVRVersionMinor << '\n';
+
+    
 }

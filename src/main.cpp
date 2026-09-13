82e81f70f61feda71566d19b03074093b2668c6c
haiazi <haiazipooks@outlook.com>
2026-09-13 06:44:46 +0000

    OpenVR Initalztion

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,20 @@
 int main (void){
     std::cout << "VR Monitor starting...\n";
 
+    vr::EVRInitError error = vr::VRInitError_None;
 
-    std::cout << "openvr version: " << vr::k_nSteamVRVersionMinor << '\n';
+    vr::IVRSystem* vrSystem = vr::VR_Init(
+        &error,
+        vr::VRApplication_Overlay
+    );
 
-    
+    if (error != vr::VRInitError_None){
+        std::cout << "OpenVR initialization failed: " << vr::VR_GetVRInitErrorAsEnglishDescription(error) << "\n";
+        return 1;
+    }
+
+    std::cout << "OpenVR initialztion successfully!\n";
+
+    vr::VR_Shutdown;
+
 }
